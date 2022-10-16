ab697fb05fb51364a6f19d1ce45cfa3fb6053ffd
tezon <tezonteam@gmail.com>
2022-10-16 02:23:28 -0700

    make it better

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 const path = require("path");
 const express = require("express");
-const dotenv = require("dotenv");
+const dotenv = require("dotenv").config();
 const { errorHandler } = require("./middleware/errorMiddleware");
 const connectDB = require("./config/db");
 const colors = require("colors");
@@ -8,7 +8,6 @@ const router = require('./routes/userRoutes')
 const port = process.env.PORT || 5000;
 const cors = require("cors");
 
-dotenv.config();
 
 connectDB();
 
