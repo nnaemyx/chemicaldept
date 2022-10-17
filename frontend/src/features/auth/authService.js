4f32522b5f3f6c636f0298898a18d1bf794c11fc
tezon <tezonteam@gmail.com>
2022-10-16 21:09:49 -0700

    make it better

--- frontend/src/features/auth/authService.js
+++ frontend/src/features/auth/authService.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_URL = "http://localhost:5000/api/register/"
+const API_URL = "/api/register/"
 
 const register = async (userData) => {
     const response = await axios.post(API_URL, userData)
