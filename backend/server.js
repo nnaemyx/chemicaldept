e456ebcda4a32e7f2b94393868bf6e86469126de
tezon <tezonteam@gmail.com>
2022-10-16 21:19:21 -0700

    make it better

--- backend/server.js
+++ backend/server.js
@@ -16,11 +16,12 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
+app.use(express.static("build"));
 
 app.use('/api/register', router)
 
 // Serve frontend
-if (process.env.NODE_ENV === "production") {
+if (process.env.PORT === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/build")));
 
   app.get("*", (req, res) =>
